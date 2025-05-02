ad38654dd24cb8a50c9d21bb92db9dfd8faab6f6
hzl <421850241@qq.com>
2025-05-02 15:30:30 +0800

    完成私信列表的发送与读取

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -168,4 +168,8 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId,headerUrl);
     }
 
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
+
 }
